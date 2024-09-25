6a21ac219244a5f4e0faa064fdf30c9bc11c9bf5
kodfun <kodluyorumtv@gmail.com>
2024-09-26 02:01:40 +0700

    degiskenler & operatorler

---
+++ ortak/yaz.js
@@ -0,0 +1,12 @@
+document.body.style.background = "black";
+document.body.style.color = "white";
+document.body.style.fontSize = "3em";
+document.body.style.fontFamily = '"Courier New", monospace';
+
+function yaz(html = "") {
+    document.body.innerHTML += html + "<br>";
+}
+
+function ciz(html = "") {
+    document.body.innerHTML += html + "<hr>";
+}
